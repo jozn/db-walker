1ea04fa13a05de1998afe0fdf4280c7d75a23c54
Hamid <7atashfeshan@gmail.com>
2018-01-20 17:31:47 +0330

    change to sun2 and new android ms/socila directories

--- src/config.go
+++ src/config.go
@@ -3,9 +3,10 @@ package src
 const (
 	TEMPLATES_DIR = `C:\Go\_gopath\src\ms\snake\templates\`
 	//OUTPUT_DIR_GO_X       = `C:\Go\_gopath\src\ms\snake\play\`
-	OUTPUT_DIR_GO_X  = `C:\Go\_gopath\src\ms\sun\models\x\`
-	OUTPUT_DIR_GO_X_CONST  = `C:\Go\_gopath\src\ms\sun\models\x\xconst\`
-	OUTPUT_PROTO_DIR = `C:\Go\_gopath\src\ms\sun\models\protos\`
+	//OUTPUT_DIR_GO_X  = `C:\Go\_gopath\src\ms\sun\models\x\`
+	OUTPUT_DIR_GO_X  = `C:\Go\_gopath\src\ms\sun2\shared\x\`
+	OUTPUT_DIR_GO_X_CONST  = `C:\Go\_gopath\src\ms\sun2\shared\x\xconst\`
+	OUTPUT_PROTO_DIR = `C:\Go\_gopath\src\ms\sun2\shared\proto\`
 	DATABASE         = "ms"
 
 	FORMAT = true
